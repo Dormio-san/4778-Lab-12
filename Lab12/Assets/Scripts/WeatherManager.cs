d04345b021a87278687ea43fbb286847493c4c75
JW419 <148804427+JW419@users.noreply.github.com>
2024-11-13 15:14:49 -0500

    weather manager updated
    here we gooooo

--- Lab12/Assets/Scripts/WeatherManager.cs
+++ Lab12/Assets/Scripts/WeatherManager.cs
@@ -6,7 +6,7 @@ using UnityEngine.Networking;
 
 public class WeatherManager
 {
-    private const string xmlApi = "http://api.openweathermap.org/data/2.5/weather?q=Orlando,us&mode=xml&appid=APIKEY";
+    private const string xmlApi = "http://api.openweathermap.org/data/2.5/weather?q=Orlando,us&mode=xml&appid=075b4b30c5afa318dae5d2f5c390071e";
 
     private IEnumerator CallAPI(string url, Action<string> callback)
     {
